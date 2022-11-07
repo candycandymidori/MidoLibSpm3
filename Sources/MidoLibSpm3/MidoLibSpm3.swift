bca9889da9abdaef3711a3ff75ed1d321e8d2640
midori <midori@candyhouse.co>
2022-11-07 16:24:53 +0800

    cc

--- Sources/MidoLibSpm3/MidoLibSpm3.swift
+++ Sources/MidoLibSpm3/MidoLibSpm3.swift
@@ -1,6 +1,17 @@
-public struct MidoLibSpm3 {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
+import Foundation
+import os.log
+public class lg {
+  public static func mido(_ items: Any..., file: String = #file, function: String = #function,
+         line: Int = #line, tag: String = "WEB midori spm =>") {
+    #if DEBUG
+    let fileName = (file as NSString).lastPathComponent
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "HH:mm:ss"
+    dateFormatter.timeZone = TimeZone(abbreviation: "Asia/Taipei")
+    dateFormatter.locale = Locale(identifier: "ja_JP")
+    let ismain = (Thread.isMainThread) ? "主線":"副線"
+    let content = "\(tag) \(dateFormatter.string(from: Date())) \(items) \(fileName):\(line) \(ismain)"
+    print(content)
+    #endif
+  }
 }
